ba6e392146b6ec6fb9f86cacfa832a5d2fc23a1f
Shreyas <shreyas.misra03@gmail.com>
2022-10-28 11:45:55 +0530

    Changed the image upload fuction

--- index.js
+++ index.js
@@ -34,51 +34,10 @@ mongoose
   .then(console.log("Connected to the sever"))
   .catch((err) => console.log(err));
 
-//Setting up CORS for sending request from a url to another url
-//Read more https://enable-cors.org/
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS"); //Allowing all the method's to be performed
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
-
 //Setting up CORS
 app.use(
   cors({ origin: [`https://bloggerjs.netlify.app`, `http://localhost:3000`] })
 );
-
-//Creating the Images folder
-// function ensureExists(path, cb) {
-//   fs.mkdir(path, (err) => {
-//     if (err) {
-//       if (err.code == "EEXIST")
-//         cb(null); // Ignore the error if the folder already exists
-//       else cb(err); // Something else went wrong
-//     } else cb(null); // Successfully created folder
-//   });
-// }
-
-// ensureExists(__dirname + "/Images", (err) => {
-//   // Handle folder creation error
-//   if (err) console.log(err);
-//   // We're all good
-//   else {
-//     //Uploading the images using multer
-//     const storage = multer.diskStorage({
-//       destination: (req, file, cb) => cb(null, "Images"),
-//       filename: (req, file, cb) => cb(null, req.body.name),
-//     });
-
-//     const upload = multer({ storage });
-//     app.post("/api/upload", upload.single("file"), (req, res) =>
-//       res.status(200).json("Image has been Uploaded!")
-//     );
-//   }
-// });
 
 //Auth Route
 app.use("/api/auth", authRoute);
