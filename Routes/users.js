6f714f147cd48454769ab758dc84a094fb2c207d
Shreyas <shreyas.misra03@gmail.com>
2022-11-09 16:00:29 +0530

    Revert change

--- Routes/users.js
+++ Routes/users.js
@@ -83,7 +83,7 @@ router.get("/:id", async (req, res) => {
 router.get("/:username", async (res, req) => {
   //Finding the author
   try {
-    const user = await User.findOne(req.params.username.replace("%20", " "));
+    const user = await User.findOne(req.params.username);
 
     //Fetching the profile picture of author
     const { profilePicture, ...props } = user._doc;
